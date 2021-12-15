371c55a440ee55d5b53ae4c08795106c516ec70e
Susantshah <susantshah@gmail.com>
2021-12-15 18:03:26 +0545

    Userselect button clcik event handled to run game

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,6 +5,14 @@ difficult is selected then difficulty.checked would be false
 else true. */
 
 const difficulty = document.querySelector('input[name="difficulty"]');
+
+//Buttons Selection
+const playerChoiceRockButton = document.querySelector(".rockbtn");
+const playerChoicePaperButton = document.querySelector(".paperbtn");
+const playerChoiceScissorButton = document.querySelector(".scissorbtn");
+const playerChoiceLizardButton = document.querySelector(".lizardbtn");
+const playerChoiceSpockButton = document.querySelector(".spockbtn");
+
 const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
 const playerSelectedItem = document.getElementById("player-choice");
 const computerSelectedItem = document.getElementById("computer-choice");
@@ -13,3 +21,36 @@ const computerOuterBox = document.getElementById("choice-outer-box-computer");
 
 // Global Variable
 let roundsCount = 0;
+let resultArray = [];
+
+playerChoiceRockButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  gamesLogic("Rock");
+});
+playerChoicePaperButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  gamesLogic("Paper");
+});
+playerChoiceScissorButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  gamesLogic("Scissor");
+});
+playerChoiceLizardButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  gamesLogic("Lizard");
+});
+playerChoiceSpockButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  gamesLogic("Spock");
+});
+
+function computerRandomChoice() {
+  const getRandomNumber = Math.floor(Math.random() * 5);
+  const computerSelectedOption = choices[getRandomNumber];
+  return computerSelectedOption;
+}
+
+const gamesLogic = (userChoice) => {
+  const computerChoice = computerRandomChoice();
+  console.log(userChoice, computerChoice);
+};
