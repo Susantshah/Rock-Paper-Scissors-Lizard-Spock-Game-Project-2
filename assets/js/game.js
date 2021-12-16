f04fcd1b8b3bc71509484613823f418b5596dc3e
Susantshah <susantshah@gmail.com>
2021-12-16 19:07:30 +0545

    All basic functionality to the game added with proper comments

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,11 @@ else true. */
 
 const difficulty = document.querySelector('input[name="difficulty"]');
 
+//Scores and Rounds Div
+const computerScoreDiv = document.getElementById("computer-score");
+const userScoreDiv = document.getElementById("user-score");
+const roundsDiv = document.getElementById("game-rounds");
+
 //Buttons Selection
 const playerChoiceRockButton = document.querySelector(".rockbtn");
 const playerChoicePaperButton = document.querySelector(".paperbtn");
@@ -32,48 +37,96 @@ const winLoseText = document.getElementById("win-or-lose");
 
 // Global Variable
 let roundsCount = 0;
-let resultArray = [];
+let userScore = 0;
+let computerScore = 0;
 
-playerChoiceRockButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  playerSelectedItem.src = rockImage;
-  playerOuterBox.classList.add("outer-box-no-blur");
-  playLogic("Rock");
-});
-playerChoicePaperButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  playerSelectedItem.src = paperImage;
-  playerOuterBox.classList.add("outer-box-no-blur");
-  playLogic("Paper");
-});
-playerChoiceScissorButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  playerSelectedItem.src = scissorImage;
-  playerOuterBox.classList.add("outer-box-no-blur");
-  playLogic("Scissor");
-});
-playerChoiceLizardButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  playerSelectedItem.src = lizardImage;
-  playerOuterBox.classList.add("outer-box-no-blur");
-  playLogic("Lizard");
-});
-playerChoiceSpockButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  playerSelectedItem.src = spockImage;
-  playerOuterBox.classList.add("outer-box-no-blur");
-  playLogic("Spock");
-});
+// Function to increment computer score when computer wins
+function incrementComputerScore() {
+  computerScore += 1;
+  computerScoreDiv.innerHTML = computerScore;
+}
 
+// Function to increment user score when user wins
+function incrementUserScore() {
+  userScore += 1;
+  userScoreDiv.innerHTML = userScore;
+}
+
+// Function to increment no of rounds player after each round
+function incrementRounds() {
+  roundsCount += 1;
+  roundsDiv.innerHTML = roundsCount;
+}
+
+/* Function to reset game if user wants to reset or any of
+user or computer score 10 first */
+function resetGame() {
+  roundsCount = 0;
+  userScore = 0;
+  computerScore = 0;
+  computerScoreDiv.innerHTML = computerScore;
+  userScoreDiv.innerHTML = userScore;
+  roundsDiv.innerHTML = roundsCount;
+}
+
+//Function to generate random choice for computer
 function computerRandomChoice() {
+  /* Select random no from 0 to 4 which
+  are treated array index of choices array
+  and selected one item randomly and returened 
+  it to calling function playLogic */
   const getRandomNumber = Math.floor(Math.random() * 5);
   const computerSelectedOption = choices[getRandomNumber];
   return computerSelectedOption;
 }
 
+// Handling click event of rock by user
+playerChoiceRockButton.addEventListener("click", (e) => {
+  e.preventDefault(); // Preventing default behaviour
+  playerSelectedItem.src = rockImage; // Changing image of user sleceted item to rock
+  playerOuterBox.classList.add("outer-box-no-blur"); // Removing Blur
+  playLogic("Rock"); // Calling PlayLogic function to handle further game
+});
+
+// Handling click event of paper by user
+playerChoicePaperButton.addEventListener("click", (e) => {
+  e.preventDefault(); // Preventing default behaviour
+  playerSelectedItem.src = paperImage; // Changing image of user sleceted item to paper
+  playerOuterBox.classList.add("outer-box-no-blur"); // Removing Blur
+  playLogic("Paper"); // Calling PlayLogic function to handle further game
+});
+
+// Handling click event of scissor by user
+playerChoiceScissorButton.addEventListener("click", (e) => {
+  e.preventDefault(); // Preventing default behaviour
+  playerSelectedItem.src = scissorImage; // Changing image of user sleceted item to scissor
+  playerOuterBox.classList.add("outer-box-no-blur"); // Removing Blur
+  playLogic("Scissor"); // Calling PlayLogic function to handle further game
+});
+
+// Handling click event of lizard by user
+playerChoiceLizardButton.addEventListener("click", (e) => {
+  e.preventDefault(); // Preventing default behaviour
+  playerSelectedItem.src = lizardImage; // Changing image of user sleceted item to lizard
+  playerOuterBox.classList.add("outer-box-no-blur");
+  playLogic("Lizard");
+});
+
+// Handling click event of spock by user
+playerChoiceSpockButton.addEventListener("click", (e) => {
+  e.preventDefault(); // Preventing default behaviour
+  playerSelectedItem.src = spockImage; // Changing image of user sleceted item to spock
+  playerOuterBox.classList.add("outer-box-no-blur"); // Removing Blur
+  playLogic("Spock"); // Calling PlayLogic function to handle further game
+});
+
+//function to handle main logic of game after user selected its choice
 const playLogic = (userChoice) => {
+  // Get computer random choice from computerRandomChoice function
   const computerChoice = computerRandomChoice();
-  computerOuterBox.classList.add("outer-box-no-blur");
+  computerOuterBox.classList.add("outer-box-no-blur"); // remove blur from computer choice box
+
+  //Selct computer selected image according to computerChoice using switch case
   switch (computerChoice) {
     case "Rock":
       computerSelectedItem.src = rockImage;
@@ -98,40 +151,61 @@ const playLogic = (userChoice) => {
     default:
       break;
   }
+
+  /* Calling checkGameWinner function with 
+  userchoice and computerchoice as argument for game result */
   checkGameWinner(userChoice, computerChoice);
+
+  //Increase no of rounds on every playLogic call
+  incrementRounds();
 };
 
+//Function to show text in gamescreen when user loses the game
 function setLoseText(userChoice, computerChoice) {
   gameResutltText.innerHTML = `${computerChoice} Beats ${userChoice}`;
   winLoseText.innerHTML = `YOU LOSE!`;
+
+  //Remove win-text class from winLoseText if exist and add lose-text class
   if (winLoseText.classList.contains("win-text")) {
     winLoseText.classList.remove("win-text");
   }
   winLoseText.classList.add("lose-text");
 
+  /* Remove outer-box-win class from playerOuterBox if exist 
+  and add outer-box-lose class */
   if (playerOuterBox.classList.contains("outer-box-win")) {
     playerOuterBox.classList.remove("outer-box-win");
   }
   playerOuterBox.classList.add("outer-box-lose");
 
+  /* Remove outer-box-lose class from computerOuterBox if exist 
+  and add outer-box-win class */
   if (computerOuterBox.classList.contains("outer-box-lose")) {
     computerOuterBox.classList.remove("outer-box-lose");
   }
   computerOuterBox.classList.add("outer-box-win");
 }
 
+//Function to show text in gamescreen when user wins the game
 function setWinText(userChoice, computerChoice) {
   gameResutltText.innerHTML = `${userChoice} Beats ${computerChoice}`;
   winLoseText.innerHTML = `YOU WIN!`;
+
+  //Remove lose-text class from winLoseText if exist and add win-text class
   if (winLoseText.classList.contains("lose-text")) {
     winLoseText.classList.remove("lose-text");
   }
   winLoseText.classList.add("win-text");
+
+  /* Remove outer-box-lose class from playerOuterBox if exist 
+  and add outer-box-win class */
   if (playerOuterBox.classList.contains("outer-box-lose")) {
     playerOuterBox.classList.remove("outer-box-lose");
   }
   playerOuterBox.classList.add("outer-box-win");
 
+  /* Remove outer-box-win class from computerOuterBox if exist 
+  and add outer-box-lose class */
   if (computerOuterBox.classList.contains("outer-box-win")) {
     computerOuterBox.classList.remove("outer-box-win");
   }
@@ -143,40 +217,59 @@ function checkGameWinner(userChoice, computerChoice) {
   if (userChoice === computerChoice) {
     gameResutltText.innerHTML = `${userChoice} Draws ${computerChoice}`;
     winLoseText.innerHTML = ``;
+
+    // Remove outer-box-win class from computerOuterBox if exist
     if (computerOuterBox.classList.contains("outer-box-win")) {
       computerOuterBox.classList.remove("outer-box-win");
     }
+
+    // Remove outer-box-win class from playerOuterBox if exist
     if (playerOuterBox.classList.contains("outer-box-win")) {
       playerOuterBox.classList.remove("outer-box-win");
     }
+
+    // Remove outer-box-lose class from playerOuterBox if exist
     if (playerOuterBox.classList.contains("outer-box-lose")) {
       playerOuterBox.classList.remove("outer-box-lose");
     }
+
+    // Remove outer-box-lose class from computerOuterBox if exist
     if (computerOuterBox.classList.contains("outer-box-lose")) {
       computerOuterBox.classList.remove("outer-box-lose");
     }
   } else if (userChoice === "Rock" && computerChoice === "Scissor") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else if (userChoice === "Rock" && computerChoice === "Lizard") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else if (userChoice === "Lizard" && computerChoice === "Spock") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else if (userChoice === "Lizard" && computerChoice === "Paper") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else if (userChoice === "Paper" && computerChoice === "Rock") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else if (userChoice === "Paper" && computerChoice === "Spock") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else if (userChoice === "Scissor" && computerChoice === "Rock") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else if (userChoice === "Scissor" && computerChoice === "Spock") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else if (userChoice === "Spock" && computerChoice === "Rock") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else if (userChoice === "Spock" && computerChoice === "Scissor") {
-    setWinText(userChoice, computerChoice);
+    setWinText(userChoice, computerChoice); // Set user win text to gamescreen
+    incrementUserScore(); //increment user score
   } else {
-    setLoseText(userChoice, computerChoice);
+    setLoseText(userChoice, computerChoice); // Set user lose text to gamescreen
+    incrementComputerScore(); //increment computer score
   }
   /* Else condtion handle all cases when user will lose the game */
 }
