6f62cb2dc55d659fd725ba38066f11dd3aaf4efa
Susantshah <susantshah@gmail.com>
2021-12-19 12:02:29 +0545

    Finished up the project in first stage

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,20 +3,16 @@ var modalButton = document.getElementById("modal-btn");
 var modalCloseButton = document.getElementById("modal-close-btn");
 var modalBox = document.getElementById("modal-box");
 
+// When View Rules button or Modal Opening button is clicked
 modalButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  modalBox.classList.remove("hide");
-  modalBox.classList.add("show");
+  e.preventDefault(); //Preventing default behaviour
+  modalBox.classList.remove("hide"); //Remove hide class from modal
+  modalBox.classList.add("show"); //Add show class from modal to show modal
 });
 
+// When Modal close button is clicked
 modalCloseButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  modalBox.classList.remove("show");
-  modalBox.classList.add("hide");
-});
-
-modalBox.addEventListener("blur", (e) => {
-  e.preventDefault();
-  modalBox.classList.remove("show");
-  modalBox.classList.add("hide");
+  e.preventDefault(); //Preventing default behaviour
+  modalBox.classList.remove("show"); //Remove show class from modal to show modal
+  modalBox.classList.add("hide"); //Add hide class from modal
 });
